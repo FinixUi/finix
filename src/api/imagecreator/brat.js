1db504365f455b07557b687c1b18293792c26b9e
FinixUi <akunpribff752@gmail.com>
2025-07-07 21:30:31 +0700

    update brat.js

--- src/api/imagecreator/brat.js
+++ src/api/imagecreator/brat.js
@@ -1,13 +1,26 @@
-module.exports = function app (app) {
-app.get('/imagecreator/brat', async (req, res) => {
+const fetch = require('node-fetch')
+
+async function brat(q) {
+    let res = await fetch(`https://brat.caliphdev.com/api/brat?text=${q}`)
+    let buffer = await res.buffer()
+    return buffer
+}
+
+module.exports = function(app) {
+    app.get('/imagecreator/brat', async (req, res) => {
+        const { text } = req.query
+        
+        if (!text) {
+            return res.status(400).json({ status: false, error: "Tolong masukkan text nya" });
+        }
+        
         try {
-            const { text } = req.query
-            const pedo = await getBuffer(`https://brat.caliphdev.com/api/brat?text=${text}`)
+            const anuge = await brat(text);
             res.writeHead(200, {
                 'Content-Type': 'image/png',
-                'Content-Length': pedo.length,
+                'Content-Length': anuge.length,
             });
-            res.end(pedo);
+            res.end(anuge);
         } catch (error) {
             res.status(500).send(`Error: ${error.message}`);
         }
